224f87ded84b451f88dca6997617861e0608256c
Lunatuna321 <Lunatuna070321@gmail.com>
2024-06-24 23:35:58 +0800

    Further testing

--- discordCounter.py
+++ discordCounter.py
@@ -2,8 +2,14 @@ import time
 from pynput.keyboard import Controller, Listener, Key
 
 keyboard = Controller()
-keyboard.press(Key.alt)
+
+keyboard.press(Key.cmd)
 keyboard.press(Key.tab)
-time.sleep(1)
-keyboard.release(Key.alt)
+keyboard.press(Key.right)
+time.sleep(0.8)
+
+keyboard.release(Key.right)
+
+
+keyboard.release(Key.cmd)
 keyboard.release(Key.tab)
